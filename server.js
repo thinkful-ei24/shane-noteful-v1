197e58f4d8f2ff478e33bc68c8666b45949a5d1d
Shane Lupton <slupton89@gmail.com>
2018-09-04 13:16:51 -0600

    loading static page and loading data

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 'use strict';
 
+console.log('Hello Noteful!');
+
+const express = require('express');
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+const app = express();
 
-// INSERT EXPRESS APP CODE HERE...
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.listen(8080, () => { console.info(`Server listening on http://127.0.0.1:8080`)
+}).on('error', err => {
+  console.error(err);
+});
