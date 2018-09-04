6a894304b492eb5325966c560d6dec7acbe808b7
Shane Lupton <slupton89@gmail.com>
2018-09-04 13:26:36 -0600

    retrieving details and setup nodemon

--- server.js
+++ server.js
@@ -14,6 +14,14 @@ app.get('/api/notes', (req, res) => {
   res.json(data);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const note = data.find(item => item.id === Number(req.params.id));
+  if(!data) {
+    return res.send('Failed');
+  }
+  return res.send(note);
+})
+
 app.listen(8080, () => { console.info(`Server listening on http://127.0.0.1:8080`)
 }).on('error', err => {
   console.error(err);
