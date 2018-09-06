b10d86b0946cfdb8430a005d55b378bd38cd3296
Shane Lupton <slupton89@gmail.com>
2018-09-06 13:38:21 -0600

    Added post endpoint

--- routers/notes.router.js
+++ routers/notes.router.js
@@ -51,4 +51,28 @@ NotesRouter.put('/:id', (req, res, next) => {
   });
 });
 
+NotesRouter.post('/', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+
+  /* Never trust users */
+  if(!newItem.title) {
+    const err = new Error(`Missing 'title' in request body`);
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if(err) {
+      return next(err);
+    }
+    if(item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
 module.exports = NotesRouter;
